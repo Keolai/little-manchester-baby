ecf122fa933c262a67bc7b7ad7b9806090bcea7b
Keolailani Rose <krose102202@gmail.com>
2025-04-26 01:59:39 -0500

    error handling fixed

--- assembler/parser.cpp
+++ assembler/parser.cpp
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void reverseString(char *);
-void intToBinaryString(int, char*);
+void intToBinaryString(int, char *);
 
 int main(int argc, char *argv[])
 {
@@ -12,10 +12,10 @@ int main(int argc, char *argv[])
     char *filename = argv[1];
     FILE *file = fopen(filename, "r"); // Open the file for reading
     char *outputName = argv[2];
-    if (outputName == NULL){
-        outputName = "output.txt"; //default
+    if (outputName == NULL)
+    {
+        outputName = "output.txt"; // default
         printf("output.txt will countain output\n");
-
     }
 
     if (file == NULL)
@@ -24,45 +24,56 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
+    bool error = false;
     char line[256]; // Buffer to store each line from the file
     int i = 0;
     char buffer[34];
     FILE *fptr;
 
-// Open a file in writing mode
+    // Open a file in writing mode
     fptr = fopen(outputName, "w");
 
-// Write some text to the file
-
-// Close the file
-    
     while (fgets(line, sizeof(line), file))
     {
         // Remove newline character if present
         line[strcspn(line, "\n")] = '\0';
 
-        int output = assign(line);
-       // printf("%d\n",output);
-        intToBinaryString(output,buffer);
-        if (i < 31){
-        buffer[32] = '\n';
-        buffer[33] = 0;
-        } else {
-        buffer[32] == 0; 
+        int output = assign(line,i,&error);
+        if (error){
+             fclose(file);
+             fclose(fptr);
+            remove(outputName);
+            printf("aborting...\n");
         }
-       // reverseString(buffer);
+        // printf("%d\n",output);
+        intToBinaryString(output, buffer);
+        if (i < 31)
+        {
+            buffer[32] = '\n';
+            buffer[33] = 0;
+        }
+        else
+        {
+            buffer[32] == 0;
+        }
+        // reverseString(buffer);
         fprintf(fptr, buffer);
-        printf(buffer);
+        // printf(buffer);
         i++;
     }
 
-    if (i < 31){
-        for (int j = 0; j < 32-i;j++){
-            //fill out rest of program with 0
-            if (j != 31 - i){
-            fprintf(fptr,"00000000000000000000000000000000\n");
-            } else {
-            fprintf(fptr,"00000000000000000000000000000000");
+    if (i < 31)
+    {
+        for (int j = 0; j < 32 - i; j++)
+        {
+            // fill out rest of program with 0
+            if (j != 31 - i)
+            {
+                fprintf(fptr, "00000000000000000000000000000000\n");
+            }
+            else
+            {
+                fprintf(fptr, "00000000000000000000000000000000");
             }
         }
     }
@@ -71,22 +82,27 @@ int main(int argc, char *argv[])
         // printf("%d\n",program[j]);
     }
     fclose(file);
-    fclose(fptr); 
+    fclose(fptr);
 
-   // run(program);
+    // run(program);
 }
 
-void intToBinaryString(int num, char* buffer){
+void intToBinaryString(int num, char *buffer)
+{
     unsigned int mask = 1U << 31;
     int i;
-    for (i = 31; i > -1; i--) {
+    for (i = 31; i > -1; i--)
+    {
         int tmp = (num & mask) ? 1 : 0;
-        if (tmp == 0){
+        if (tmp == 0)
+        {
             buffer[i] = '0';
-        } else {
+        }
+        else
+        {
             buffer[i] = '1';
         }
         num <<= 1;
-        //printf("%c",buffer[i]);
+        // printf("%c",buffer[i]);
     }
 }
